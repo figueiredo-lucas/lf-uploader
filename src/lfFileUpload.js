8884480d14cdc4ba6ae37b39ece72dc36520111c
Lucas Figueiredo <ccomp.lucas@gmail.com>
2016-10-29 07:56:46 -0200

    Melhoria na apresentação e passagem do objeto

--- src/lfFileUpload.js
+++ src/lfFileUpload.js
@@ -3,10 +3,10 @@ angular.module("lfUploader").directive("lfFileUpload", function () {
         restrict: 'E',
         replace: true,
         template: ['<div class="file-upload-container">',
-            '   <div class="pic-upload" id="pup" ng-hide="!!model" ng-click="uploadPic()"></div>',
-            '   <input type="file" name="fup" id="fup" ng-hide="true"/>',
-            '   <span ng-show="!!model">',
-            '       <img class="uploaded-pic" id="pic" ng-src="{{model}}" alt="Foto carregada" />',
+            '   <div class="pic-upload" id="pup" ng-hide="!!thumb" ng-click="uploadPic()"></div>',
+            '   <input accept="image/*" type="file" name="fup" id="fup" ng-hide="true"/>',
+            '   <span ng-show="!!thumb">',
+            '       <img class="uploaded-pic" id="pic" ng-src="{{thumb}}" alt="Foto carregada" />',
             '       <span id="close">&times;</span>',
             '   </span>',
             ' </div>'
@@ -23,15 +23,17 @@ angular.module("lfUploader").directive("lfFileUpload", function () {
                 };
                 file.onchange = function () {
                     if (this.files && this.files[0]) {
+                        scope.model = this.files[0];
                         var reader = new FileReader();
                         reader.onload = function (e) {
-                            scope.model = e.target.result;
+                            scope.thumb = e.target.result;
                             scope.$apply();
                         };
                         reader.readAsDataURL(this.files[0]);
                     }
                 };
                 close.onclick = function () {
+                    scope.thumb = null;
                     scope.model = null;
                     file.value = '';
                     scope.$apply();
